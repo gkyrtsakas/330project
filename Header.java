0383f6ba59d6e6b6c2572f44c1ffd4643a877e85
gsakis <gkyrtsakas@gmail.com>
2013-08-11 15:45:41 -0400

    Header

---
+++ Header.java
@@ -0,0 +1,54 @@
+package project;
+
+public class Header	{
+	private int pid;
+	private int priority;
+	private int submitTime;
+	private int waitTime;
+	private int cpuTime;
+	private int completeTime;
+	private int sizeInRAM;
+	private boolean deviceRequest;
+	
+	public Header (int pid, int prio, int sub, int size, boolean devReq){
+		this.pid = pid;
+		this.priority = prio;
+		this.submitTime = sub;
+		this.sizeInRAM = size;
+		this.deviceRequest = devReq;
+		
+		this.waitTime = 0;
+		this.cpuTime = 0;
+		this.completeTime = 0;
+	}
+	
+	public int getPid(){
+		return pid;
+	}
+	
+	public int getSubmitTime (){
+		return submitTime;
+	}
+	
+	public int getWaitTime (){
+		return waitTime;
+	}
+	
+	public int getCompleteTime (){
+		return completeTime;
+	}
+	
+	public void setCompleteTime (int time){
+		this.completeTime = time;
+	}
+	
+	public void incWait(){
+		this.waitTime++;
+	}
+	
+	public void incCPU(){
+		this.cpuTime++
+	}
+	
+	
+}
