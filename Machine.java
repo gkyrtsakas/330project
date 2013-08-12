038bd2ec1a2a2e3732bb87d5078e1a4791a3399c
gsakis <gkyrtsakas@gmail.com>
2013-08-12 10:44:31 -0400

    idkifthiswillwork

---
+++ Machine.java
@@ -0,0 +1,21 @@
+package project;
+
+public class Machine {
+	private int[] regs;
+	private long maxRAM = 3435973868; 	// 4GB RAM in bits
+	private int pageSize = 65535;		// 8KB page size
+	private long clock;
+	
+	
+	private static final Machine instance = new Machine(); //run one machine at a time
+	private Machine() {
+		
+	}
+	
+	public static Machine getInstance(){
+		return instance;
+	}
+	
+
+	
+}
