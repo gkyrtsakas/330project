9ce5744c6e9189777a8e6f9335c4a25edbc3e91d
Mairhu <starping07@hotmail.com>
2013-08-18 14:43:24 -0400

    Device & Resource
    Updated Device and Resource classes to display tables and allow for
    searching

--- Device.java
+++ Device.java
@@ -1,39 +1,119 @@
 import java.util.*;
 
 public class Device {
-	private static ArrayList<Device> devices = new ArrayList<Device>();
-	private String deviceName;
-	private int currentProcess;
-	private boolean locked;
-	
+	protected static ArrayList<Device> devices = new ArrayList<Device>();
+	protected static int count = 0;
+	protected String portID;
+	protected String deviceName;
+	protected Process currentProcess;
+	protected boolean locked;
 	
 	Device(String name){
 		this.deviceName = name;
 		locked = false;
 		devices.add(this);
+		count ++;
+		portID = "D-" + count;
 	}
 	
-	ArrayList<Device> getAllDevices(){
+	static ArrayList<Device> getAllDevices(){
 		return devices;
+	}
+	
+	static String getTableInfo(){
+		String isLocked = "", processName = "", deviceName = "", portID = "", 
+		returnString = "|-------Device Name-------|---Port ID---|-----Current Process-----|--Locked--|";
+		for(int x=0; x < count; x++){
+			Device d = devices.get(x);
+			// format locked string
+			if(d.isAvailable()){
+				isLocked = "Available.";
+			}
+			else{
+				isLocked = "Locked....";
+			}
+			// format Device name string
+			deviceName = d.deviceName;
+			for(int i = deviceName.length(); i < 25; i++){
+				deviceName += ".";
+			}
+			
+			// format port ID string
+			portID = d.portID;
+			for(int i = portID.length(); i < 13; i++){
+				portID += ".";
+			}
+		
+			// format process name string
+			if(d.currentProcess != null){
+				processName = d.currentProcess.getName();
+				for(int i = processName.length(); i < 25; i++){
+					processName += ".";
+				}
+			}
+			else{
+				processName = ".........................";
+			}
+			
+			
+			returnString += "\n|" + deviceName + "|" + portID + "|" + processName + "|" + isLocked + "|";
+		}
+		return returnString;
+	}
+	
+	static Device findDevice(String devicePort){
+		int index = Integer.parseInt((devicePort.split("-"))[1]);
+		if(index <= count){
+			return devices.get(index - 1);
+		}
+		else{
+			System.out.println("Error: There is no device with this port");
+			return null;
+		}
 	}
 	
 	String getName(){
 		return deviceName;
 	}
 	
-	void setLock(int newProcess){
-		locked = true;
-		this.currentProcess = newProcess;
-		
+	void setLock(Process newProcess){
+		if(!locked){
+			locked = true;
+			this.currentProcess = newProcess;
+		}
+		else{
+			System.out.println("Error: Locked");
+		}
 	}
 	
 	void releaseLock(){
 		locked = false;
-		this.currentProcess = (Integer)null;
+		this.currentProcess = (Process)null;
 	}
 	
 	boolean isAvailable(){
 		return !locked;
+	}
+	
+	public static void main(String args[]){
+		Vector<Integer> x = new Vector<Integer>(1);
+		Process p1 = new Process(1, "The process", 2, 3, false, x);
+		Process p2 = new Process(2, "The Thing", 3, 3, false, x);
+		Device d = new Device("Hi there I'mma device");
+		d.setLock(p1);
+		d = new Device("Hi there I'mma device2");
+		d = new Device("Hi there I'mma device3");
+		d.setLock(p2);
+		d = new Device("Hi there I'mma device4");
+		System.out.println(Device.getTableInfo());
+		d = Device.findDevice("D-3");
+		if(d != null){
+			System.out.println("D-3 found");
+		}
+		d = Device.findDevice("D-23");
+		if(d != null){
+			System.out.println("D-23 found");
+		}
 	}
 
 }
--- Resource.java
+++ Resource.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 public class Resource {	
 	private static ArrayList<Resource> resources = new ArrayList<Resource>();
+	private static int count = 0;
+	private String portID;
 	private String resourceName;
-	private int currentProcess;
+	private Process currentProcess;
 	private boolean locked;
 	
 	
@@ -11,25 +13,96 @@ public class Resource {
 		this.resourceName = name;
 		locked = false;
 		resources.add(this);
+		count ++;
+		portID = "R-" + count;
 	}
 	
-	ArrayList<Resource> getAllResources(){
+	static ArrayList<Resource> getAllResources(){
 		return resources;
 	}
 	
-	void setLock(int newProcess){
-		locked = true;
-		this.currentProcess = newProcess;
+	static Resource findDevice(String resourcePort){
+		int index = Integer.parseInt((resourcePort.split("-"))[1]);
+		if(index <= count){
+			return resources.get(index - 1);
+		}
+		else{
+			System.out.println("Error: There is no device with this port");
+			return null;
+		}
+	}
+	
+	static String getTableInfo(){
+		String isLocked = "", processName = "", resourceName = "", portID = "", 
+		returnString = "|------Resource Name------|---Port ID---|-----Current Process-----|--Locked--|";
+		for(int x=0; x < count; x++){
+			Resource r = resources.get(x);
+			// format locked string
+			if(r.isAvailable()){
+				isLocked = "Available.";
+			}
+			else{
+				isLocked = "Locked....";
+			}
+			// format Device name string
+			resourceName = r.resourceName;
+			for(int i = resourceName.length(); i < 25; i++){
+				resourceName += ".";
+			}
+			
+			// format port ID string
+			portID = r.portID;
+			for(int i = portID.length(); i < 13; i++){
+				portID += ".";
+			}
 		
+			// format process name string
+			if(r.currentProcess != null){
+				processName = r.currentProcess.getName();
+				for(int i = processName.length(); i < 25; i++){
+					processName += ".";
+				}
+			}
+			else{
+				processName = ".........................";
+			}
+			
+			
+			returnString += "\n|" + resourceName + "|" + portID + "|" + processName + "|" + isLocked + "|";
+		}
+		return returnString;
+	}
+	
+	void setLock(Process newProcess){
+		if(!locked){
+			locked = true;
+			this.currentProcess = newProcess;
+		}
+		else{
+			System.out.println("Error: Locked");
+		}
 	}
 	
 	void releaseLock(){
 		locked = false;
-		this.currentProcess = (Integer)null;
+		this.currentProcess = (Process)null;
 	}
 	
 	boolean isAvailable(){
 		return !locked;
+	}
+	
+	public static void main(String args[]){
+		Vector<Integer> x = new Vector<Integer>(1);
+		Process p1 = new Process(1, "The process", 2, 3, false, x);
+		Process p2 = new Process(2, "The Thing", 3, 3, false, x);
+		Resource r = new Resource("Hi there I'mma device");
+		r.setLock(p1);
+		r = new Resource("Hi there I'mma device2");
+		r = new Resource("Hi there I'mma device3");
+		r.setLock(p2);
+		r = new Resource("Hi there I'mma device4");
+		System.out.println(Resource.getTableInfo());
 	}
 
 }
