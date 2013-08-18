c34467caa4905965d0baec15d75af649e1e8d6c5
siriuscord <jayjamesbennett@gmail.com>
2013-08-18 15:33:51 -0400

    added waiting queue handling

--- NonPreemptiveScheduler.java
+++ NonPreemptiveScheduler.java
@@ -99,4 +99,58 @@ public class NonPreemptiveScheduler
 		queue.add(process);		
 		return queue;
 	}
+	
+	public LinkedList moveFromRunningToWaiting(Process process, LinkedList<Process> queue)
+	{
+		queue.add(process);		
+		return queue;
+	}
+	
+	public LinkedList moveFromWaitingToReady(Process process, LinkedList<Process> queue)
+	{
+		//FCFS Algorithm
+		if (scheduleType == 1)
+		{
+			queue.add(process);
+		}
+		//SJF Algorithm
+		else if (scheduleType == 2)
+		{
+			boolean flag = false;
+			
+			for (int k=0; k<=queue.size(); k++)
+			{
+				if (process.getBurstsCycle().size() < queue.get(k).getBurstsCycle().size())
+				{
+					queue.add(k, process);
+				}
+				flag = true;
+			}
+			
+			if (flag==false)
+			{
+				queue.add(process);
+			}
+		}
+		//Priority Algorithm
+		else if (scheduleType == 3)
+		{
+			boolean flag = false;
+			
+			for (int k=0; k<=queue.size(); k++)
+			{
+				if (process.getPriority() < queue.get(k).getPriority())
+				{
+					queue.add(k, process);
+				}
+				flag = true;
+			}
+			
+			if (flag==false)
+			{
+				queue.add(process);
+			}
+		}
+		return queue;
+	}
 }
