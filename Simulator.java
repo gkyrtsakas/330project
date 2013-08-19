7c9403823c29d9822e81caba9d040dc50890eb02
gsakis <gkyrtsakas@gmail.com>
2013-08-19 13:56:40 -0400

    kworknowthanks

--- Simulator.java
+++ Simulator.java
@@ -16,17 +16,29 @@ public class Simulator implements Runnable {
 	}
 	
 	//create objects of every other class.
-	QueueManager queue = new QueueManager(1);
-	queue.setDefaultDeviceTable();
-	
-	//						pid name pri subtime  burst	dev req		res req
-	Process p1 = new Process(1,"p1", 1, 	0, 		6, 	"D-1 D-2", "R-1 R-3");
-	Process p2 = new Process(2,"p2", 2, 	3, 		1,  "D-4 D-2", "R-2 R-5");
-	Process p3 = new Process(3,"p3", 1, 	3, 		3,  "D-1 D-2", "R-1 R-3");
-	Process p4 = new Process(4,"p4", 3, 	4, 		2,  "D-8 D-9", "R-2 R-4");
-	Process p5 = new Process(5,"p3", 1, 	6, 		9,  "D-1 D-2", "R-1 R-2");
+
 
 	public void run() {
+		Machine comp = Machine.getInstance();
+		
+		QueueManager queue = new QueueManager(1);
+		queue.setDefaultDeviceTable();
+		queue.setDefaultResourceTable();
+		
+		//						pid name pri subtime  burst	dev req		res req
+		Process p1 = new Process(1,"p1", 1, 	0, 		6, 	"D-1 D-2", "R-1 R-3");
+		Process p2 = new Process(2,"p2", 2, 	3, 		1,  "D-4 D-2", "R-2 R-5");
+		Process p3 = new Process(3,"p3", 1, 	3, 		3,  "D-1 D-2", "R-1 R-3");
+		Process p4 = new Process(4,"p4", 3, 	4, 		2,  "D-8 D-9", "R-2 R-4");
+		Process p5 = new Process(5,"p3", 1, 	6, 		9,  "D-1 D-2", "R-1 R-2");
+		
+		queue.addProcess(p1);
+		queue.addProcess(p2);
+		queue.addProcess(p3);
+		queue.addProcess(p4);
+		queue.addProcess(p5);
+		
+		
 		int response = 1;
 		String input;
 		
@@ -40,7 +52,6 @@ public class Simulator implements Runnable {
 			System.out.println("3. Add I/O Device");
 			System.out.println("4. Run Simulator");
 			System.out.println("5. Change Scheduling Type");
-			System.out.println("6. Print Last Run Statistics");
 			
 			@SuppressWarnings("resource")
 			Scanner reader = new Scanner(System.in);
@@ -61,13 +72,14 @@ public class Simulator implements Runnable {
 				System.out.println("Resource Added...");
 				break;
 			case 4:
-				//run sim
+				while (true){
+					queue.update();
+					queue.toString();
+					queue.cycleIncrement();
+				}
 				break;
 			case 5:
 				//change scheduling
-				break;
-			case 6:
-				//print stats
 				break;
 			default:
 				break;
